74e52e98d5361a6a58f1c8bc3e878b24e54621a6
smb_smn <italexmail@rambler.ru>
2024-11-03 19:54:51 +0500

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # stuff_2_blog
+Материалы для блога https://teletype.in/@velipre_xella
---
+++ common_scripts/create_table_scott_emp.sql
@@ -0,0 +1,10 @@
+create table scott.emp (
+	empno int4 not null,
+	ename varchar(10) null,
+	job varchar(9) null,
+	mgr int4 null,
+	hiredate date null,
+	sal int4 null,
+	comm int4 null,
+	deptno int4 null
+);
